6b9a73ed5e9cf1180e6cbb1df6ac04cad4e5f51a
wvbe <wybe@x-54.com>
2023-12-23 14:13:45 +0100

    Port the good ol' pizza place

---
+++ src/mess/classes/Scenario.ts
@@ -0,0 +1,61 @@
+import * as THREE from 'three';
+import { Controller } from './Controller';
+import { Event } from './Event';
+const PREFER_DARK_MODE = false;
+
+export class Scenario extends Controller {
+	start() {
+		console.group('Setup scenario');
+		this.setCameraPosition(new THREE.Vector3(0, 0, -6));
+		this.setCameraFocusOnVector3(new THREE.Vector3(0, 0, 0));
+		this.setLightMode(this.darkMode);
+		this.setStageProps();
+		this.startAnimationLoop();
+		console.groupEnd();
+	}
+
+	public darkMode = PREFER_DARK_MODE;
+
+	$light = new Event();
+
+	setLightMode(nightlight: boolean) {
+		this.darkMode = nightlight;
+		globalThis.document.body.setAttribute('class', this.darkMode ? 'nightlight' : 'daylight');
+		this.$light.emit();
+	}
+
+	toggleLightMode() {
+		this.setLightMode(!this.darkMode);
+	}
+
+	async setStageProps() {
+		this.scene.add(new THREE.AmbientLight(0xffffff, 0.3));
+
+		const directional1 = new THREE.DirectionalLight(0xffffff, 1.8);
+		directional1.position.set(1, 1, 1).normalize();
+		this.scene.add(directional1);
+
+		const directional2 = new THREE.DirectionalLight(0xffffff, 2.7);
+		directional2.position.set(-1, 1, -1).normalize();
+		this.scene.add(directional2);
+
+		const gltf = await this.addGltf('gltf/pizza-low-res.gltf');
+
+		const applyMaterialToMesh = () => {
+			const material = new THREE.MeshPhongMaterial({
+				color: this.darkMode ? 0xcccccc : 0x333333,
+				wireframe: false,
+				shininess: 100,
+			});
+			gltf.scene.traverse((o) => {
+				if (o instanceof THREE.Mesh) {
+					o.material = material;
+				}
+			});
+		};
+
+		applyMaterialToMesh();
+
+		this.$detach.on(this.$light.on(applyMaterialToMesh));
+	}
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,77 @@
-import { FC } from 'react';
+import { FC, useCallback, useEffect, useRef } from 'react';
+import styles from './index.module.css';
+import './index.css';
+
+import { Scenario } from '@/mess/classes/Scenario';
 
 const Derp: FC = () => {
-	return <main>Derp</main>;
+	const scenario = useRef<Scenario | null>(null);
+	const startThreeScenario = useCallback((element: HTMLDivElement | null) => {
+		if (!element) {
+			return;
+		}
+		scenario.current = new Scenario(element, {
+			enableAutoRotate: false,
+			enablePan: false,
+			enableZoom: true,
+			fieldOfView: 45,
+			pixelRatio: window.devicePixelRatio || 1,
+			restrictCameraAngle: false,
+		});
+
+		scenario.current.start();
+	}, []);
+
+	const flipDarkMode = useCallback(() => {
+		if (!scenario.current) {
+			return;
+		}
+		scenario.current.setLightMode(!scenario.current.darkMode);
+	}, []);
+
+	return (
+		<div>
+			<div className={styles.three} ref={startThreeScenario}></div>
+
+			<p className={styles.text}>
+				wybe minnebo
+				<br />
+				built in 1988
+				<br />
+				webdevelopment since 2001
+			</p>
+
+			<a className={`${styles.nightlight} ${styles.button}`} onClick={flipDarkMode}>
+				Day turns night
+			</a>
+
+			<div className={styles.ui}>
+				<p>
+					<a
+						href="https://github.com/wvbe?tab=repositories"
+						className={styles.button}
+						title="GitHub is where I keep my public code, click here to visit."
+					>
+						code
+					</a>{' '}
+					<a
+						href="https://www.instagram.com/wvvbe/"
+						className={styles.button}
+						title="Instagram is where I upload some of my prettier photos, click here to visit."
+					>
+						photo
+					</a>{' '}
+					<a
+						href="https://www.linkedin.com/in/wybe/"
+						className={styles.button}
+						title="LinkedIn is where you can find my professional life, click here to visit."
+					>
+						werk
+					</a>
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Derp;
