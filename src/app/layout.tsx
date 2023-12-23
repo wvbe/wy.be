635c8b48689c31cdf58f292122090b5b8bc7593f
wvbe <wybe@x-54.com>
2023-12-23 20:19:38 +0100

    :)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,34 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './layout.css';
 import styles from './layout.module.css';
 import Link from 'next/link';
 
-const inter = Inter({ subsets: ['latin'] });
-
 export const metadata: Metadata = {
 	title: 'https://wy.be',
 	description: 'My personal website, v2024',
+	creator: 'Wybe',
+	robots: 'index, follow',
 };
+
+const menuLinks = ['/', '/privacy-policy'];
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body>
 				<div className={styles.wrapper}>
-					<nav>
-						<Link href={'/'}>/</Link>
-						<Link href={'/privacy-policy'}>/privacy-policy</Link>
+					<nav className={styles.nav}>
+						{menuLinks.sort().map((link) => (
+							<Link key={link} href={link}>
+								{link}
+							</Link>
+						))}
 					</nav>
 
 					{children}
+					<aside className={styles.copyright}>
+						whatever I said, if it was smart it copyrighted
+					</aside>
 				</div>
 			</body>
 		</html>
--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,12 +1,20 @@
-import styles from './page.module.css';
 import { FC } from 'react';
 
 const Derp: FC = () => {
 	return (
 		<main>
 			<h1>Privacy policy</h1>
-			<p>I am profoundly uninterested in your data, or maintaining ways to acquire it.</p>
-			<p>This site does not collect or store any of your data.</p>
+			<p>
+				<b>
+					This document describes the data handling for https://wy.be and all related platforms, for
+					users from anywhere.
+				</b>
+			</p>
+
+			<p>
+				This site does not collect any data for any purpose. It does not store, resell, transmute,
+				discombobulate or otherwise handle data directly or via business partners.
+			</p>
 		</main>
 	);
 };
